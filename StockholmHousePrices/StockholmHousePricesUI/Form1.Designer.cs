1824d6a107e7ef1c908e120440d0cf8a9817982b
ximovai <ximocoder@users.noreply.github.com>
2017-12-12 19:57:57 +0100

    created parse method & UI changes

---
+++ StockholmHousePrices/StockholmHousePricesUI/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace StockholmHousePricesUI
+{
+    partial class FormPrices
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonClose = new System.Windows.Forms.Button();
+            this.ButtonScrap = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonClose
+            // 
+            this.ButtonClose.Location = new System.Drawing.Point(819, 513);
+            this.ButtonClose.Name = "ButtonClose";
+            this.ButtonClose.Size = new System.Drawing.Size(75, 23);
+            this.ButtonClose.TabIndex = 0;
+            this.ButtonClose.Text = "Close";
+            this.ButtonClose.UseVisualStyleBackColor = true;
+            this.ButtonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // ButtonScrap
+            // 
+            this.ButtonScrap.Location = new System.Drawing.Point(12, 513);
+            this.ButtonScrap.Name = "ButtonScrap";
+            this.ButtonScrap.Size = new System.Drawing.Size(75, 23);
+            this.ButtonScrap.TabIndex = 1;
+            this.ButtonScrap.Text = "Scrap";
+            this.ButtonScrap.UseVisualStyleBackColor = true;
+            this.ButtonScrap.Click += new System.EventHandler(this.ButtonScrap_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(882, 495);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(706, 514);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormPrices
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 548);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ButtonScrap);
+            this.Controls.Add(this.ButtonClose);
+            this.Name = "FormPrices";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prices";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonClose;
+        private System.Windows.Forms.Button ButtonScrap;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StockholmHousePrices/StockholmHousePricesUI/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using HtmlAgilityPack;
+using SQLite;
+
+namespace StockholmHousePricesUI
+{
+    public partial class FormPrices : Form
+    {
+        public FormPrices()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ButtonScrap_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Clear();
+            this.richTextBox1.Update();
+            this.Scrap();
+        }
+
+        private void Scrap()
+        {
+            var html = @"https://www.hemnet.se/bostader?location_ids%5B%5D=17744&living_area_min=80&fee_max=7000&price_max=2000000";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+
+            var pagename = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+
+            var houses = htmlDoc.DocumentNode.SelectNodes("//div[@data-item-id]");
+
+            List<House> housesList = new List<House>();
+            foreach (var house in houses)
+            {
+                House houseObject = new House();
+
+                var id = int.Parse(house.SelectSingleNode(".//img[@class='property-image']").Attributes.AttributesWithName("onerror").FirstOrDefault().DeEntitizeValue.Split('"')[5].Replace("ID", ""));
+                var prize = house.SelectSingleNode(".//li[@class='price item-result-meta-attribute-is-bold']").InnerText.Trim();
+                var size = house.SelectSingleNode(".//li[@class='living-area item-result-meta-attribute-is-bold']").InnerText.Trim();
+                var city = house.SelectSingleNode(".//li[@class='city item-result-meta-attribute-subtle']").InnerText.Trim();
+                var rooms = house.SelectSingleNode(".//li[@class='rooms item-result-meta-attribute-is-bold']").InnerText.Trim();
+                var pricem2 = house.SelectSingleNode(".//li[@class='price-per-m2 item-result-meta-attribute-subtle']").InnerText.Trim();
+                var area = house.SelectSingleNode(".//li[@class='area item-result-meta-attribute-subtle']").InnerText.Trim();
+
+                houseObject.Id = id;
+                houseObject.Name = id.ToString();
+                houseObject.Prize = int.Parse(prize.Replace("kr", "").Trim().Replace(" ", string.Empty));
+                houseObject.Size = size;
+                houseObject.Area = area;
+                houseObject.Location = city;
+
+                housesList.Add(houseObject);
+            }
+
+            var prices = houses.Descendants();
+
+            // Get an absolute path to the database file
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "HousePrices.db");
+            var db = new SQLiteConnection(databasePath);
+
+            Console.WriteLine("Node Name: " + pagename.Name + "\n" + pagename.OuterHtml);
+
+            foreach (var ohouse in housesList)
+            {
+                string line = "House: " + ohouse.Id.ToString() + " - Price " + ohouse.Prize + " - location: " +
+                              ohouse.Location + " / " + ohouse.Area + " - size: " + ohouse.Size +
+                              " - link: " + " https://www.hemnet.se/bostad/" + ohouse.Id.ToString();
+                Console.WriteLine(line);
+                this.richTextBox1.Text += line + Environment.NewLine;
+                this.AddHouse(db, ohouse);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get an absolute path to the database file
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "HousePrices.db");
+
+            var db = new SQLiteConnection(databasePath);
+            //db.CreateTable<House>();
+            //db.CreateTable<Valuation>();
+            var query = db.Table<House>();
+        }
+
+        private void AddHouse(SQLiteConnection db, House house)
+        {
+            var s = db.Insert(house);
+        }
+    }
+
+
+    public class House
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        public string Area { get; set; }
+        public string Name { get; set; }
+        public int Prize { get; set; }
+        public string Size { get; set; }
+        public string Location { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
+
